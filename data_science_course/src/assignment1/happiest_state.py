2d9662c1935fb790056144b71e52f7104afac730
seekme94 <owais.a.hussain@gmail.com>
2014-07-24 02:15:35 +0500

    Assignment 2 files added

--- data_science_course/src/assignment1/happiest_state.py
+++ data_science_course/src/assignment1/happiest_state.py
@@ -6,20 +6,35 @@ import json
 def out (x):
     print(str(x).encode('cp850', errors='replace'))
 
+def calculate_tweet_score (tweet_text, scores):
+    words = tweet_text.split(" ")
+    sent_score = 0.0
+    for word in words:
+      word = ''.join(e for e in word if e.isalnum())  # Removes all special characters from a word
+      score = scores.get(word.lower())  # Turn to lower case for better matches
+      if score != None:
+        sent_score = sent_score + int(score)  # Add score to total sentiment score
+    return sent_score
+
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
 
-    states = {'USA','AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'}
-    happiness = {}  # initialize an empty dictionary
+    states = {'USA', 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'}
+    happiness = {}  # Initialize empty dictionary
     tweets = {}
     scores = {}
-    for state in states:
-        happiness[state] = 0.0
+
+    # Save term scores
     for line in sent_file:
         term, score = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
         scores[term] = int(score)  # Convert the score to an integer.
 
+    # Initiate happiness score of each state
+    for state in states:
+        happiness[state] = 0.0
+
+    # Scan every tweet; process those having place
     for line in tweet_file:
         tweet = json.loads(line)  # Returns a dict object into tweet
         if tweet.__contains__("text"):
@@ -31,11 +46,18 @@ def main():
                 place = json.loads(place_dump)
                 if place != None:
                     country_code = place.get("country_code")
-                    if country_code == 'US': # Since only residents of USA have sentiments
+                    if country_code == 'US':  # Since only residents of USA have sentiments
                         full_name = place.get("full_name")
                         city, state = full_name.split(", ")
-                        happiness[state] = happiness[state] + 0 # Compute tweet sentiment
-            tweets[tweet_id] = tweet_text
-
+                        tweet_sent = float(calculate_tweet_score(tweet_text, scores))  # Compute tweet sentiment
+                        happiness[state] = happiness[state] + tweet_sent
+    max_score = 0.0
+    happy_state = ''
+    for state in happiness:
+        if happiness[state] > max_score:
+            happy_state = state
+            max_score = happiness[state]
+#        print(state + " " + str(happiness[state]))
+    print(happy_state)
 if __name__ == '__main__':
     main()
